68229bfdb12acaf5b9d43419f6d2aaeab0f01dbe
Udit <uthakkar@gammastack.com>
2023-02-17 12:08:19 +0530

    booo

--- file2.rb
+++ file2.rb
@@ -1,1 +1,10 @@
 puts "file2"
+
+# variables
+
+name = "John"
+age = "100"
+
+puts ("There once was a man named " + name)
+puts ("he was " + age + " years old.")
+puts ("He was sad as he was " + age + " years old")
