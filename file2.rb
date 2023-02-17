eef7da648d3a234f5a0f0df9edf9caf469853de6
Udit <uthakkar@gammastack.com>
2023-02-17 15:19:47 +0530

    Done with inputs

--- file2.rb
+++ file2.rb
@@ -43,3 +43,14 @@ puts 10%3
 #  convert no to string
 num = 20 
 puts ("hello " + num.to_s)
+
+# taking input
+# puts "Enter your name"
+# name = gets.chomp() 
+# puts ("Hello " + name)
+
+puts "Enter a number"
+num1 = gets.chomp().to_i
+puts "Enter a number"
+num2 = gets.chomp().to_i
+puts (num1 + num2)
