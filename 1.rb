00d4ef3516be12ac2d8e49804812bda0ba31be0e
nsaini007 <nsaini@gammastack.com>
2023-02-17 12:13:23 +0530

    rebased to ns from main

---
+++ 1.rb
@@ -0,0 +1,28 @@
+#printing
+puts "hello world"
+print "world hello"
+
+name = "Mike"
+age = 17
+gpa = 7.5
+is_tall = true
+
+puts "The name of person is " + name
+puts "His age is #{age}"
+print "gpa : #{gpa}"
+print " Is he tall #{true}"
+
+# puts prints in new line whereas print prints in same line
+
+#TypeCasting
+
+#doesn't work not c/c++
+# num = 'b' - 'a'
+# puts num
+
+puts 3.14.to_i
+puts 3.to_f
+puts 3.to_s
+
+
+
