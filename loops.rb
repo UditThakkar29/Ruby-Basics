6e6bdc57c50ab5f56c3a28b5eee44d1c71c8bb1a
Udit <uthakkar@gammastack.com>
2023-02-17 19:20:40 +0530

    Loops

---
+++ loops.rb
@@ -0,0 +1,34 @@
+# While loop
+
+x = 5
+while x>=2
+  puts x
+  x = x-1
+end
+
+# FOR LOOP
+
+for a in 1..4 do
+  puts a
+end
+
+arr = [1,3,4,"array","hash","range"]
+for i in arr do
+  puts i
+end
+
+# DO WHILE LOOP
+
+loop do
+  puts "Hello"
+  val = 10
+  if val == 10
+    break
+  end
+end
+
+num = 2
+until num == 5 do
+  puts num
+  num = num + 1
+end
